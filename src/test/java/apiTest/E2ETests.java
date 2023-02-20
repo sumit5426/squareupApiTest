bd5b423cba98636d307f64e9ffb8374f45bd6ce6
Sourav Das <souravdasbitm@gmail.com>
2023-02-20 11:07:26 +0530

    Fist Test Case

--- src/test/java/apiTest/E2ETests.java
+++ src/test/java/apiTest/E2ETests.java
@@ -3,18 +3,23 @@ package apiTest;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
 
 public class E2ETests {
 
     public static void main(String[] args) {
-        String baseUrl="https://data.binance.com";
+        String baseUrl = "https://45.114.246.163";
 
-        RestAssured.baseURI=baseUrl;
+        RestAssured.baseURI = baseUrl;
         RequestSpecification request = RestAssured.given();
-        request.header("Content-Type", "application/json");
+        request.header("accept", "*/*");
+        request.header("designationId", "0001");
+        request.header("ulbCode", "ULB1");
+        Response response = request.get("/squareup_sna_subcomponents/web/umgmt/employees");
+        //System.out.println(response.statusCode());
+        //System.out.println(response.statusCode());
+        //System.out.println(response.getBody().prettyPrint());
+        Assert.assertEquals("Status code",200,response.statusCode());
 
-        Response response = request.get("/api/v3/ticker/24hr");
-
-        System.out.println(response.getBody().prettyPrint());
     }
 }
