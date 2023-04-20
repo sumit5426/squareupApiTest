3859ca81f24d261333e4c3f534d65b9ea05006de
Sourav Das <souravdasbitm@gmail.com>
2023-04-20 11:15:37 +0530

    Refactored

--- src/test/java/stepDefinitions/EmployeeOperation.java
+++ src/test/java/stepDefinitions/EmployeeOperation.java
@@ -12,13 +12,14 @@ import java.util.Map;
 public class EmployeeOperation {
     private static Response response;
     private static final String baseUrl = "https://squareupuate.com/squareupdevwosubcomponentsbe";
-    private static final String empId="auto_" + LocalDate.now() + Math.random() ;
+    private static final String empId = "auto_" + LocalDate.now() + Math.random();
+    private static final String token = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiZXRraWFkbWluIiwiZXhwIjoxNjgxODAzNzQ1LCJpYXQiOjE2ODE3ODkzNDV9.Ah-7Ohi5JOHkF-v7zACCRXS5G02i3P7awj9tybKmzi61EtwCjucaJNCKhnFA28oqn2I9lhpvLf9CFKUO4n80KA";
 
     public static void getEmployeeList() {
 
         //data Layer from cucumber feature file
         Map<String, Object> headerMap = new HashMap<>();
-        headerMap.put("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiZXRraWFkbWluIiwiZXhwIjoxNjgxODAzNzQ1LCJpYXQiOjE2ODE3ODkzNDV9.Ah-7Ohi5JOHkF-v7zACCRXS5G02i3P7awj9tybKmzi61EtwCjucaJNCKhnFA28oqn2I9lhpvLf9CFKUO4n80KA");
+        headerMap.put("Authorization", token);
         headerMap.put("accept", "*/*");
         headerMap.put("Host", "squareupuate.com");
         headerMap.put("designationId", "0");
@@ -47,10 +48,10 @@ public class EmployeeOperation {
 
         //data Layer from cucumber feature file
 
-        String payload = "{\"employeeId\":\""+empId+"\",\"name\":\"aaaa\",\"mobileNumber\":\"9999999999\",\"email\":\"aa@leadwinner.com\",\"address\":\"Hyderabad\",\"designationId\":\"4\",\"gender\":\"Male\",\"profilePicture\":\"\",\"employeeULBDesignationEntities\":[{\"ulbCode\":\"87940\",\"designationId\":4,\"activeStatus\":true}]}";
+        String payload = "{\"employeeId\":\"" + empId + "\",\"name\":\"aaaa\",\"mobileNumber\":\"9999999999\",\"email\":\"aa@leadwinner.com\",\"address\":\"Hyderabad\",\"designationId\":\"4\",\"gender\":\"Male\",\"profilePicture\":\"\",\"employeeULBDesignationEntities\":[{\"ulbCode\":\"87940\",\"designationId\":4,\"activeStatus\":true}]}";
 
         Map<String, Object> headerMap = new HashMap<>();
-        headerMap.put("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiZXRraWFkbWluIiwiZXhwIjoxNjgxODAzNzQ1LCJpYXQiOjE2ODE3ODkzNDV9.Ah-7Ohi5JOHkF-v7zACCRXS5G02i3P7awj9tybKmzi61EtwCjucaJNCKhnFA28oqn2I9lhpvLf9CFKUO4n80KA");
+        headerMap.put("Authorization", token);
         headerMap.put("accept", "*/*");
         headerMap.put("Content-Type", "application/json");
         headerMap.put("Host", "squareupuate.com");
@@ -70,10 +71,35 @@ public class EmployeeOperation {
 
     }
 
+    public static void getEmployee() {
+
+        //data Layer from cucumber feature file
+        Map<String, Object> headerMap = new HashMap<>();
+        headerMap.put("Authorization", token);
+        headerMap.put("accept", "*/*");
+        headerMap.put("Host", "squareupuate.com");
+
+        Map<String, Object> queryParamMap = new HashMap<>();
+        queryParamMap.put("employeeId", empId);
+
+        // Action of the test layer
+        RestAssured.baseURI = baseUrl;
+        RequestSpecification request = RestAssured.given();
+        request.headers(headerMap);
+        request.queryParams(queryParamMap);
+        response = request.get("/web/umgmt/employees/view");
+
+        // Validation of the test layer
+        System.out.println(response.statusCode());
+        System.out.println(response.asPrettyString());
+
+    }
+
     public static void main(String[] args) {
 
 
         createEmployee();
+        getEmployee();
         getEmployeeList();
 
 
